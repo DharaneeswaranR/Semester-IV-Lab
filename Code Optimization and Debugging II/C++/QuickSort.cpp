fce0e5dbeba44432fcae38887ac17fdb019c1c71
DharaneeswaranR <dharaneeshram@gmail.com>
2022-01-24 14:42:58 +0530

    Added: QuickSort(Python, C++, C)

---
+++ Code Optimization and Debugging II/C++/QuickSort.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+void swap(int *x, int *y) {
+    int t = *x;
+    *x = *y;
+    *y = t;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(&arr[j], &arr[i]);
+        }
+    }
+
+    swap(&arr[i+1], &arr[high]);
+
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi-1);
+        quickSort(arr, pi+1, high);
+    }
+}
+
+
+int main() {
+    int size = 6;
+    int arr[size];
+
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    quickSort(arr, 0, size-1);
+
+    for (int num: arr) {
+        cout << num << " ";
+    }
+}
+
+/*
+Input: 5 2 6 3 9 4
+Output: 2 3 4 5 6 9
+*/
---
+++ Code Optimization and Debugging II/C/QuickSort.py
@@ -0,0 +1,28 @@
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+
+    return i + 1
+
+def quick_sort(arr, low, high):
+    if low < high:
+        pi = partition(arr, low, high)
+        quick_sort(arr, low, pi-1)
+        quick_sort(arr, pi+1, high)
+
+arr = [int(x) for x in input().split()]
+quick_sort(arr, 0, len(arr)-1)
+
+print(*arr)
+
+'''
+Note: Replace input() with sys.stdin.readline() (needs to import sys) if output prints the input
+
+'''
